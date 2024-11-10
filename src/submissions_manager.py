8e52cb66f75dda58bcdc6625a388c6b72b9f01c0
Alessandro Bernocchi <alex.bernocchi.ab@gmail.com>
2024-11-10 19:31:25 +0100

    added logger support

--- src/submissions_manager.py
+++ src/submissions_manager.py
@@ -7,26 +7,32 @@ import time
 from src.judge import execute_code_locally
 from src.config_loader import load_config
 from src.util import get_auth_headers
+from src.logger import get_logger
 
 config = load_config()
+logger = get_logger()
 
 async def callback(message: aio_pika.IncomingMessage):
     async with message.process():
         try:
             data = json.loads(message.body.decode())
-            
+            logger.info(f"Received submission: {data}")
             if 'code' not in data:
-                print('code not specified')
+                logger.error('code not specified')
+                # print('code not specified')
                 return
             if 'problem_id' not in data:
-                print('problem_id not specified')
+                logger.error('problem_id not specified')
+                # print('problem_id not specified')
                 return
             if 'language' not in data:
-                print('language not specified')
+                logger.error('language not specified')
+                # print('language not specified')
                 return
 
             if 'submission_id' not in data:
-                print('submission_id not specified')
+                logger.error('submission_id not specified')
+                # print('submission_id not specified')
                 return
 
             execution_result = execute_code_locally(data['code'], data['problem_id'], data['language'], data['submission_id'])
@@ -49,16 +55,19 @@ async def callback(message: aio_pika.IncomingMessage):
                     pass
 
             if not sent:
-                print(f'Failed in sending submission {data['submission_id']}')
+                logger.error(f'Failed in sending submission {data["submission_id"]}')
+                # print(f'Failed in sending submission {data['submission_id']}')
 
         except Exception as e:
-            print(e)
+            logger.error(f'Error while processing submission: {e}')
+            # print(e)
 
 async def start_listen_for_submissions():
     while True:
         try:
             # Attempt to connect to RabbitMQ
-            print("Trying to connect to RabbitMQ...")
+            logger.info("Trying to connect to RabbitMQ...")
+            # print("Trying to connect to RabbitMQ...")
             connection = await aio_pika.connect_robust(f'amqp://{config['rabbitmq_user']}:{config['rabbitmq_pass']}@{config['rabbitmq_host']}/')
             
             # If connection is successful, proceed
@@ -73,15 +82,18 @@ async def start_listen_for_submissions():
                 await queue.consume(callback)
 
                 # Keep the connection open for consumption
-                print(" [*] Waiting for messages. To exit press CTRL+C")
+                # print(" [*] Waiting for messages. To exit press CTRL+C")
+                logger.info(" [*] Waiting for messages...")
                 await asyncio.Future()  # Infinite loop to keep the script running
 
         except aio_pika.exceptions.AMQPConnectionError as e:
-            print(f"Failed to connect to RabbitMQ: {e}")
-
-            print(f"Retrying in 5 seconds...")
+            logger.error(f"Failed to connect to RabbitMQ: {e}")
+            # print(f"Failed to connect to RabbitMQ: {e}")
+            logger.info("Retrying in 5 seconds...")
+            # print(f"Retrying in 5 seconds...")
             await asyncio.sleep(5)
 
         except Exception as e:
-            print(f"Unexpected error: {e}")
+            logger.error(f"Unexpected error: {e}")
+            # print(f"Unexpected error: {e}")
             await asyncio.sleep(5)  # Retry after some delay in case of an unexpected error
